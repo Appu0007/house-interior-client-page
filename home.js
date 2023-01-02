27d43dc66c1acdd3932ffa21886dec6618b2800d
Appu0007 <appoosabhi143@gmai.com>
2023-01-02 17:07:49 +0530

    first commit

--- home.js
+++ home.js
@@ -1,4 +1,19 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+function myFunction(y) {
+    y.classList.toggle("change");
+    document.getElementById("nav-details").style.display="block";
   }
- 
+
+var count = 0;
+var btn = document.getElementById('humburger');
+function tapFunction(){
+  var x= ++count;
+  if(x==0){
+    document.getElementById("nav-details").style.display="none";
+  }
+  if(x%2==0){
+    document.getElementById("nav-details").style.display="none";
+  }
+  else{
+    document.getElementById("nav-details").style.display="block";
+  }
+}
